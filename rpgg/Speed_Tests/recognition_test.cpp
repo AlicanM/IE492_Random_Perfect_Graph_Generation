1bcdda291a54ab5a0fe812102e161725cf301b68
AlicanM <alicanmilani@hotmail.com>
2023-03-19 10:47:37 +0000

    recognitioon speed test

--- rpgg/Speed_Tests/recognition_test.cpp
+++ rpgg/Speed_Tests/recognition_test.cpp
@@ -7,32 +7,38 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    igraph_integer_t vcount = 50;
-    igraph_real_t density = 0.3;
+    //igraph_integer_t vertex_count[] = {200, 500, 1000, 5000};
+    igraph_integer_t vertex_count[] = {20, 40, 45, 50};
+    int size_vertex_count = 4;
+    igraph_real_t density[] = {0.2, 0.4, 0.6, 0.8};
+    int size_density = 4;
 
-time_t timer;
-
-    //for(){
-        igraph_t graph;
-        igraph_erdos_renyi_game(&graph, IGRAPH_ERDOS_RENYI_GNP,  vcount, density, IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
-        //igraph_bipartite_game (&graph, nullptr, IGRAPH_ERDOS_RENYI_GNP, 30000, 70000, 0.3, 1, IGRAPH_UNDIRECTED, IGRAPH_ALL);
+    time_t timer;
+    cout << "vertex\tdensity\tigraph\toylum" << endl; 
+    for(int v = 0; v < size_vertex_count; v++){
+        for(int d = 0; d < size_density; d++){
+            cout <<  vertex_count[v] << "\t" << density[d] << "\t";
+            igraph_t graph;
+            //igraph_erdos_renyi_game(&graph, IGRAPH_ERDOS_RENYI_GNP,  vertex_count[v], density[d], IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
+            igraph_bipartite_game (&graph, nullptr, IGRAPH_ERDOS_RENYI_GNP, vertex_count[v]/2, vertex_count[v]/2, density[d], 1, IGRAPH_UNDIRECTED, IGRAPH_ALL);
         
-        igraph_bool_t isPerfect;
+            igraph_bool_t isPerfect;
 timer = time(NULL);
-        igraph_is_perfect(&graph, &isPerfect);
+            igraph_is_perfect(&graph, &isPerfect);
 timer = time(NULL) - timer;
-        cout << timer << endl;
+            cout << timer << "\t";
 
-        PerfectnessChecker pChecker;
-        pChecker.ReadGraph(&graph);
+            PerfectnessChecker pChecker;
+            pChecker.ReadGraph(&graph);
 
 timer = time(NULL);
-	    isPerfect = pChecker.CheckPerfectness();
+	        isPerfect = pChecker.CheckPerfectness();
 timer = time(NULL) - timer;
-        cout << timer << endl;
+            cout << timer << endl;
 
-        igraph_destroy(&graph);
-    //}
+            igraph_destroy(&graph);
+        }
+    }
     
     return 0;
 }
