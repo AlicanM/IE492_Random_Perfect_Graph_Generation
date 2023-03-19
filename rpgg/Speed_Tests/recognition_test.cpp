24316bafdc70d1acefa6031523ade12752d0420b
AlicanM <alicanmilani@hotmail.com>
2023-03-19 16:02:32 +0000

    speed test mckay addition

--- rpgg/Speed_Tests/recognition_test.cpp
+++ rpgg/Speed_Tests/recognition_test.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <ctime>
 #include <oylum.h>
+#include <graphconverter.h>
 
 using namespace std;
 
@@ -10,8 +11,8 @@ int main(int argc, char *argv[])
     //igraph_integer_t vertex_count[] = {200, 500, 1000, 5000};
     igraph_integer_t vertex_count[] = {20, 40, 45, 50};
     int size_vertex_count = 4;
-    igraph_real_t density[] = {0.2, 0.4, 0.6, 0.8};
-    int size_density = 4;
+    igraph_real_t density[] = {0.2};
+    int size_density = 1;
 
     time_t timer;
     cout << "vertex\tdensity\tigraph\toylum" << endl; 
@@ -20,8 +21,12 @@ int main(int argc, char *argv[])
             cout <<  vertex_count[v] << "\t" << density[d] << "\t";
             igraph_t graph;
             //igraph_erdos_renyi_game(&graph, IGRAPH_ERDOS_RENYI_GNP,  vertex_count[v], density[d], IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
-            igraph_bipartite_game (&graph, nullptr, IGRAPH_ERDOS_RENYI_GNP, vertex_count[v]/2, vertex_count[v]/2, density[d], 1, IGRAPH_UNDIRECTED, IGRAPH_ALL);
-        
+            //igraph_bipartite_game (&graph, nullptr, IGRAPH_ERDOS_RENYI_GNP, vertex_count[v]/2, vertex_count[v]/2, density[d], 1, IGRAPH_UNDIRECTED, IGRAPH_ALL);
+            GraphConverter graph_conv;
+            string str = graph_conv.getRandomMcKayGraph(v+6);
+            cout << "." << str << "." << endl;
+            GraphConverter::graph6_to_igraph(str, &graph);
+
             igraph_bool_t isPerfect;
 timer = time(NULL);
             igraph_is_perfect(&graph, &isPerfect);
