82361d599ce194294185158a3bcf87d33d54857e
FatihMeYilmaz <fatih.yilmaz1@boun.edu.tr>
2023-04-16 21:27:29 +0000

    v2 recog test

--- rpgg/Tests/recognition_test_v2.cpp
+++ rpgg/Tests/recognition_test_v2.cpp
@@ -1,47 +1,54 @@
 #include <igraph.h>
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <string>
-#include <chrono>
-#include <iomanip>
-#include <oylum.h>
-#include <graphconverter.h>
+#include <ctime>
+#include <sstream>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    vector<string> graph6_strings;
-    ifstream infile("mc_bfs_output_100_0.4.g6");
-    string line;
-    while (getline(infile, line)) {
-        graph6_strings.push_back(line);
+    igraph_integer_t vertex_count[] = {200, 500, 1000, 5000};
+    int size_vertex_count = 4;
+    igraph_real_t density[] = {0.2, 0.4, 0.6};
+    int size_density = 3;
+
+    time_t timer;
+    cout << "vertex\tdensity\tigraph\toylum" << endl; 
+    for(int v = 0; v < size_vertex_count; v++){
+        for(int d = 0; d < size_density; d++){
+            cout <<  vertex_count[v] << "\t" << density[d] << "\t";
+
+            // Read the graph from the input file
+            igraph_t graph;
+            ifstream input_file(argv[1]);
+            if (!input_file.is_open()) {
+                cerr << "Error: could not open input file " << argv[1] << endl;
+                return 1;
+            }
+            stringstream graph_stringstream;
+            graph_stringstream << input_file.rdbuf();
+            input_file.close();
+            igraph_read_graph_graph6(&graph, graph_stringstream.str().c_str());
+
+            // Check if the graph is perfect using igraph
+            igraph_bool_t isPerfect;
+            timer = time(NULL);
+            igraph_is_perfect(&graph, &isPerfect);
+            timer = time(NULL) - timer;
+            cout << timer << "\t";
+
+            // Check if the graph is perfect using Oylum's algorithm
+            PerfectnessChecker pChecker;
+            pChecker.ReadGraph(&graph);
+            timer = time(NULL);
+            isPerfect = pChecker.CheckPerfectness();
+            timer = time(NULL) - timer;
+            cout << timer << endl;
+
+            igraph_destroy(&graph);
+        }
     }
-
-    cout << "Graph\t\t\t\t\tigraph\toylum" << endl;
-    for (const auto& graph6_string : graph6_strings) {
-        cout << graph6_string << "\t";
-        igraph_t graph;
-        GraphConverter::graph6_to_igraph(graph6_string, &graph);
-
-        igraph_bool_t isPerfect;
-        auto start = chrono::steady_clock::now();
-        igraph_is_perfect(&graph, &isPerfect);
-        auto end = chrono::steady_clock::now();
-        auto elapsed = chrono::duration_cast<chrono::microseconds>(end - start);
-        cout << fixed << setprecision(2) << elapsed.count() / 1000.0 << "ms\t";
-
-        PerfectnessChecker pChecker;
-        pChecker.ReadGraph(&graph);
-        start = chrono::steady_clock::now();
-        isPerfect = pChecker.CheckPerfectness();
-        end = chrono::steady_clock::now();
-        elapsed = chrono::duration_cast<chrono::microseconds>(end - start);
-        cout << fixed << setprecision(2) << elapsed.count() / 1000.0 << "ms" << endl;
-
-        igraph_destroy(&graph);
-    }
-
+    
     return 0;
 }
