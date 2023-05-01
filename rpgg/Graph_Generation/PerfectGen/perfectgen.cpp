38c16d629d7cdcbad18ca9cddba7c1009e667c30
Aral Dörtoğul <aral@thekompanion.com>
2023-05-01 18:03:40 +0000

    PerfectGen: bugfix clique identification

--- rpgg/Graph_Generation/PerfectGen/perfectgen.cpp
+++ rpgg/Graph_Generation/PerfectGen/perfectgen.cpp
@@ -1,5 +1,7 @@
 #include <perfectgen.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int const PerfectGen::totalOpCount = 6;
 
@@ -7,80 +9,118 @@ int const PerfectGen::totalOpCount = 6;
 // We randomly select one vertex from G and one vertex from G', and extend each one to a maximal clique, say K1 and K2 . Without loss of generality, say |K1| ≤ |K2|. We randomly choose |K1| vertices from K2 and identify them with those in K1. The bijection f to identify those vertices is randomly determined.
 void PerfectGen::cliqueIdentification(igraph_t *result, igraph_t *left, igraph_t *right) {
 
-    igraph_t *graph[2] = {left, right};
-    igraph_integer_t graph_size[2], clique_size[2], v[2], *no;
-    igraph_vector_int_t cliqueSubset[2], *maximalClique[2];
-    igraph_vector_int_list_t maximalCliqueList[2];
-    *no = 1;
+    igraph_t *graphs[2] = {left, right};
+    igraph_integer_t size[2], vertex[2];
+    igraph_vector_int_t randomMaximalClique[2];
 
+    // Part 1: Finding random cliques K1 and K2 in the graphs
     for (int i = 0; i < 2; i++) {
         // Get sizes of graphs
-        graph_size[i] = igraph_vcount(graph[i]);
+        size[i] = igraph_vcount(graphs[i]);
 
         // Choose random vertices v1 in left graph and v2 in right graph
-        v[i] = rand() % graph_size[i];
-
-        // Initialize clique subsets
-        igraph_vector_int_init(&cliqueSubset[i], 1);
-        VECTOR(cliqueSubset[i])[0] = v[i];
+        vertex[i] = rand() % size[i];
+//      std::cout << "Vertex "  << i+1 << ": " << vertex[i] << std::endl;
 
         // Get maximal cliques of the vertices
-        igraph_vector_int_list_init(&maximalCliqueList[i], 0);
-        igraph_maximal_cliques_subset(graph[i], &cliqueSubset[i], &maximalCliqueList[i], no, NULL, 0, 0);
+        igraph_vector_int_list_t allMaximalCliques;
+        igraph_integer_t maximalCliqueCount;
+        igraph_vector_int_list_init(&allMaximalCliques, 0);
 
-        // Get a random maximal clique from the maximal clique lists
-        maximalClique[i] = igraph_vector_int_list_get_ptr(&maximalCliqueList[i],  rand() % igraph_vector_int_list_size(&maximalCliqueList[i]));
+        // Get all maximal cliques of graphs
+        igraph_maximal_cliques(graphs[i], &allMaximalCliques, 0, 0);
+        
+        maximalCliqueCount = igraph_vector_int_list_size(&allMaximalCliques);
+        
+        // Detect maximal cliques that contain v1 and v2
+        igraph_vector_int_t cliquesWithTheVertex;
+        igraph_vector_int_init(&cliquesWithTheVertex, 0);
+        for (int j = 0; j < maximalCliqueCount; j++) {
+            if (igraph_vector_int_contains(igraph_vector_int_list_get_ptr(&allMaximalCliques, j), vertex[i])) {
+                igraph_vector_int_push_back(&cliquesWithTheVertex, j);
+            }
+        }
 
-        // Get clique sizes
-        clique_size[i] = igraph_vector_int_size(maximalClique[i]);
+        // Get random cliques
+        igraph_integer_t randomMaximalCliqueIndex = VECTOR(cliquesWithTheVertex)[rand() % igraph_vector_int_size(&cliquesWithTheVertex)];
+        igraph_vector_int_init_copy(&randomMaximalClique[i], igraph_vector_int_list_get_ptr(&allMaximalCliques, randomMaximalCliqueIndex));
+
+        igraph_vector_int_list_destroy(&allMaximalCliques);
+        igraph_vector_int_destroy(&cliquesWithTheVertex);
+/* 
+        std::cout << "Random clique " << i+1 <<  " with " << vertex[i] << ":" << std::endl;
+        for (int j = 0; j < igraph_vector_int_size(&randomMaximalClique[i]); j++) {
+            std::cout << VECTOR(randomMaximalClique[i])[j] << " ";
+        }
+        std::cout << std::endl;
+*/
     }
-    igraph_integer_t smallClique = (clique_size[0] < clique_size[1] ? 0 : 1);
-    igraph_integer_t largeClique = (clique_size[0] < clique_size[1] ? 1 : 0);
-    // 0: graph with larger clique, 1: graph with smaller clique
 
-    // random map
-    igraph_vector_int_shuffle(maximalClique[smallClique]);
-    igraph_vector_int_shuffle(maximalClique[largeClique]);
+    // Part 2: Generating result graph
+    igraph_integer_t cliqueSizes[2] = {igraph_vector_int_size(&randomMaximalClique[0]), igraph_vector_int_size(&randomMaximalClique[1])};
+    igraph_integer_t smallClique = (cliqueSizes[0] <= cliqueSizes[1] ? 0 : 1), largeClique = (cliqueSizes[0] <= cliqueSizes[1] ? 1 : 0);
 
-    igraph_disjoint_union(result, graph[smallClique], graph[largeClique]);
-    igraph_vector_int_add_constant(maximalClique[largeClique], graph_size[smallClique]);
+    std::vector<int> indices;
+    for (int i = 0; i < cliqueSizes[largeClique]; i++) {
+        indices.push_back(i);
+    }
+    std::random_shuffle(indices.begin(), indices.end());
+
+ 
+    std::cout << "Large clique - " << (largeClique == 0 ? "left graph " : "right graph") << "(size "<< cliqueSizes[largeClique] << ")" << std::endl;
+    for (std::vector<int>::const_iterator i = indices.cbegin(); i != indices.cend(); i++) {
+        std::cout << 1 + VECTOR(randomMaximalClique[largeClique])[*i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "Small clique - " << (smallClique == 0 ? "left graph " : "right graph")  << "(size "<< cliqueSizes[smallClique] << ")" << std::endl;
+    for (int i = 0; i < cliqueSizes[smallClique]; i++) {
+        std::cout << 1 + VECTOR(randomMaximalClique[smallClique])[i] << " ";
+    }
+    std::cout << std::endl;
+
+
+    igraph_disjoint_union(result, left, right);
+    igraph_vector_int_add_constant(&randomMaximalClique[1], size[0]);
 
     igraph_vector_int_t edges;
     igraph_vector_int_init(&edges, 0);
-    igraph_integer_t k = 0;
-    for (igraph_integer_t i = 0; i < clique_size[smallClique]; i++) {
-        igraph_integer_t smallCliqueVertex = igraph_vector_int_get(maximalClique[smallClique], i);
-        igraph_integer_t largeCliqueVertex = igraph_vector_int_get(maximalClique[largeClique], i);
+    for (igraph_integer_t i = 0; i < cliqueSizes[smallClique]; i++) {
+        igraph_integer_t smallCliqueVertex = igraph_vector_int_get(&randomMaximalClique[smallClique], i);
+        igraph_integer_t largeCliqueVertex = igraph_vector_int_get(&randomMaximalClique[largeClique], indices[i]);
+
+        // Get neighbors of the vertex in the small clique
         igraph_vector_int_t neighbors;
         igraph_vector_int_init(&neighbors, 0);
 
         igraph_neighbors(result, &neighbors, smallCliqueVertex, IGRAPH_ALL);
         igraph_integer_t neighborCount = igraph_vector_int_size(&neighbors);
+
         
+  
+//      std::cout << "Vertex " << 1+smallCliqueVertex << " - Neigbor count: " << neighborCount << std::endl;
         for (igraph_integer_t j = 0; j < neighborCount; j++)  {
             igraph_integer_t neighbor = VECTOR(neighbors)[j];
-            VECTOR(edges)[k++] = largeCliqueVertex;
-            VECTOR(edges)[k++] = neighbor;
+//          std::cout << "neighbor: " << 1+neighbor << std::endl;
+            igraph_vector_int_push_back(&edges, largeCliqueVertex);
+            igraph_vector_int_push_back(&edges, neighbor);
         }
 
-        // Free memory
-        igraph_vector_int_destroy(&neighbors);
+        igraph_vector_int_destroy(&neighbors); // Free memory
     }
-
+/* 
+    std::cout << "Edges to be added:" << std::endl;
+    for (int i = 0 ; i < igraph_vector_int_size(&edges); i+=2) {
+        std::cout << VECTOR(edges)[i] << "-" << VECTOR(edges)[i+1] << " ";
+    }
+    std::cout << std::endl;
+*/
     // Add edges
     igraph_add_edges(result, &edges, 0);
 
     // Remove vertices that are from the small clique
-    igraph_delete_vertices(result, igraph_vss_vector(maximalClique[smallClique]));
+    igraph_delete_vertices(result, igraph_vss_vector(&randomMaximalClique[smallClique]));
 
-    // Free memory
-    igraph_vector_int_destroy(&edges);
-
-    for (int i = 0; i < 2; i++) {
-        igraph_vector_int_destroy(&cliqueSubset[i]);
-        igraph_vector_int_list_destroy(&maximalCliqueList[i]);
-    }
-
+    igraph_vector_int_destroy(&edges); // Free memory
 }
 
 // Let G1, G2 be disjoint graphs, v be a vertex of G1, and N the set of all neighbors of v in G1.
@@ -112,8 +152,8 @@ void PerfectGen::substitution(igraph_t *result, const igraph_t *left, const igra
     for (igraph_integer_t i = 0; i < right_vcount; i++) {
         for (igraph_integer_t j = 0; j < neighborCount; j++) {
             igraph_integer_t neighbor = VECTOR(neighbors)[j];
-            VECTOR(edges)[k++] = (neighbor > v ? neighbor - 1 : neighbor);
-            VECTOR(edges)[k++] = left_vcount + i;
+            igraph_vector_int_push_back(&edges, (neighbor > v ? neighbor - 1 : neighbor));
+            igraph_vector_int_push_back(&edges, left_vcount + i);
         }
     }
 
@@ -171,8 +211,8 @@ void PerfectGen::composition(igraph_t *result, const igraph_t *left, const igrap
         for (igraph_integer_t j = 0; j < neighbor2Count; j++) {
             igraph_integer_t neighbor1 = VECTOR(neighbors1)[i];
             igraph_integer_t neighbor2 = VECTOR(neighbors2)[j];
-            VECTOR(edges)[k++] = (neighbor1 > v1 ? neighbor1 - 1 : neighbor1);
-            VECTOR(edges)[k++] = left_vcount + (neighbor2 > v2 ? neighbor2 - 1 : neighbor2);
+            igraph_vector_int_push_back(&edges, (neighbor1 > v1 ? neighbor1 - 1 : neighbor1));
+            igraph_vector_int_push_back(&edges, left_vcount + (neighbor2 > v2 ? neighbor2 - 1 : neighbor2));
         }
     }
 
