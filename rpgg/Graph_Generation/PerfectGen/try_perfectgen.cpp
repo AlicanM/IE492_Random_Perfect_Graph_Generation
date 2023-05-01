f459f21ec2dbf3399d01e83ad5c29d2454bc10c4
Aral Dörtoğul <aral@thekompanion.com>
2023-05-01 18:03:59 +0000

    PerfectGen: improve main function

--- rpgg/Graph_Generation/PerfectGen/try_perfectgen.cpp
+++ rpgg/Graph_Generation/PerfectGen/try_perfectgen.cpp
@@ -13,13 +13,7 @@
     #include <sys/stat.h> // For UNIX-like systems
 #endif
 
-int main(int argc, char *argv[]) {
-    srand(time(0));
-
-    igraph_t graph[2], result;
-    std::string g6[2], g6_result;
-    igraph_integer_t size[2], op;
-
+bool parseArguments(int argc, char *argv[], igraph_integer_t size[], igraph_integer_t *op) {
     if (argc < 3) {
         std::cerr << "Usage: perfectgen <size of the first graph> <size of the second graph> <combinatorial operation (optional)>" << std::endl
         << "<combinatorial operation> is optional, PerfectGen algorithm will choose a random operation if it is not given.\n\n<combinatorial operation> can be one of the following values:" << std::endl
@@ -29,18 +23,50 @@ int main(int argc, char *argv[]) {
         << "3\tDisjoint Union" << std::endl
         << "4\tJoin" << std::endl
         << "5\tComplement" << std::endl;
-        return 1;
+        return false;
     }
-    
     size[0] = atoi(argv[1]);
     size[1] = atoi(argv[2]);
 
     if (argc == 3) { // operation not given
-        op = rand() % PerfectGen::totalOpCount;
+        *op = rand() % PerfectGen::totalOpCount;
     }
     else { // operation given
-        op = atoi(argv[3]);
+        *op = atoi(argv[3]);
     }
+    return true;
+
+}
+
+void printAdjacencyMatrix(igraph_t *graph) {
+    igraph_integer_t size = igraph_vcount(graph);
+
+    igraph_matrix_t adjacency_matrix;
+    igraph_matrix_init(&adjacency_matrix, size, size);
+
+    igraph_get_adjacency(graph, &adjacency_matrix, IGRAPH_GET_ADJACENCY_BOTH, NULL, IGRAPH_NO_LOOPS);
+
+    for (long int i = 0; i < size; i++) {
+        for (long int j = 0; j < size; j++) {
+            std::cout << MATRIX(adjacency_matrix, i, j) << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    igraph_matrix_destroy(&adjacency_matrix);
+}
+
+int main(int argc, char *argv[]) {
+    srand(time(0));
+
+    igraph_t graph[2], result;
+    std::string g6[2], g6_result;
+    igraph_integer_t size[2], op;
+    
+    // Parse command line arguments
+    if(!parseArguments(argc, argv, size, &op))
+        return -1;
+    
     std::cout << "size1:\t" << size[0] << "\tsize2:\t" << size[1] << "\top:\t" << op << std::endl;
 
     // Get random McKay graphs and run PerfectGen
@@ -50,10 +76,14 @@ int main(int argc, char *argv[]) {
     if (op != 5) {
         g6[1] = GraphConverter::getRandomMcKayGraph(size[1]);
         GraphConverter::graph6_to_igraph(g6[1], &graph[1]);
-        std::cout << "graph1:\t" << g6[0] << std::endl << "graph2:\t" << g6[1] << std::endl;
+        std::cout << "graph1:\t" << g6[0] << std::endl;
+        printAdjacencyMatrix(&graph[0]);
+        std::cout << "graph2:\t" << g6[1] << std::endl;
+        printAdjacencyMatrix(&graph[1]);
         PerfectGen::perfectGen(&result, op, &graph[0], &graph[1]);
     }
     else {
+        std::cout << "graph1:\t" << g6[0] << std::endl;
         PerfectGen::perfectGen(&result, op, &graph[0], NULL);
     }
 
@@ -61,6 +91,7 @@ int main(int argc, char *argv[]) {
 
     igraph_integer_t result_size = igraph_vcount(&result);
     std::cout << "A graph with size " << result_size << " is generated." << std::endl;
+    printAdjacencyMatrix(&result);
 
     g6_result = GraphConverter::igraph_to_graph6(&result);
 
